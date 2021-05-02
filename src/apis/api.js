300a1923ee5c96b14dfec0c398697279b2e1eaa7
Joshmatjjen <joshmatparrot@gmail.com>
2021-05-02 23:41:24 +0100

    adjusted getting post api

--- src/apis/api.js
+++ src/apis/api.js
@@ -2,12 +2,18 @@ import Axios from "axios";
 import { REACT_APP_API, REACT_APP_GET_POSTS } from "@env";
 // import { Platform } from "react-native";
 
-export const getPostsApi = async (token) => {
+export const getPostsApi = async (category, page) => {
   // const headers = {
   //   "Content-Type": "application/json",
   //   Authorization: token,
   // };
-  const url = REACT_APP_API + REACT_APP_GET_POSTS + "?per_page=10000";
+  const url =
+    REACT_APP_API +
+    REACT_APP_GET_POSTS +
+    "?filter[meta_key]=category&filter[meta_value]=" +
+    category +
+    "&order=DESC&per_page=10&page=" +
+    page;
   console.log(url);
   // const collectionsMap = await Axios.get(url, { headers });
   const collectionsMap = await Axios.get(url);
