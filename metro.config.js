8d87e8477f93a792a59e273613844fec16146ed3
Joshmatjjen <joshmatparrot@gmail.com>
2021-05-18 08:58:50 +0100

    fix bug

--- metro.config.js
+++ metro.config.js
@@ -14,3 +14,11 @@
 //     },
 //   };
 // })();
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+// Remove all console logs in production...
+config.transformer.minifierConfig.compress.drop_console = true;
+
+module.exports = config;
